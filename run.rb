a4ec8f1eff26353407681a7a6a9c800c080d3c7d
Stephen Paul Weber <singpolyma@singpolyma.net>
2010-07-21 20:07:04 +0000

    All of runner in EM for config

--- run.rb
+++ run.rb
@@ -1,34 +1,43 @@
-require 'lib/nntp_server'
+$: << File.dirname(__FILE__) + '/lib'
+require 'nntp_server'
 require 'optparse'
 
-Kernel::class_eval(open('config.rb').read)
-
-OptionParser.new do |opts|
-	opts.banner = "Usage: run.rb [options]"
-
-	opts.on("-p", "--port [PORT]", Integer, "Specify port") do |v|
-		PORT = v
-	end
-
-	opts.on("-d", "--daemonize", "Daemonize server") do |v|
-		DAEMONIZE = true
-	end
-
-	opts.on("-f", "--foreground", "Run server in foreground") do |v|
-		DAEMONIZE = false
-	end
-
-	opts.on("-h", "--help", "This help text") do |v|
-		puts opts
-		exit
-	end
-end.parse!
-
-if DAEMONIZE
-	require 'daemons'
-	Daemons.daemonize
-end
-
 EventMachine::run {
+	eval(open(
+		if File.readable?(File.dirname(__FILE__) + '/config.rb')
+			File.dirname(__FILE__) + '/config.rb'
+		else
+			ENV['XDG_CONFIG_DIRS'].split(/:/).unshift('/etc').select {|dir|
+				File.readable?(dir + '/xnntpd/config.rb')
+			}.first + '/xnntpd/config.rb'
+		end
+	).read)
+
+	OptionParser.new do |opts|
+		opts.banner = "Usage: run.rb [options]"
+
+		opts.on("-p", "--port [PORT]", Integer, "Specify port") do |v|
+			PORT = v
+		end
+
+		opts.on("-d", "--daemonize", "Daemonize server") do |v|
+			DAEMONIZE = true
+		end
+
+		opts.on("-f", "--foreground", "Run server in foreground") do |v|
+			DAEMONIZE = false
+		end
+
+		opts.on("-h", "--help", "This help text") do |v|
+			puts opts
+			exit
+		end
+	end.parse!
+
+	if DAEMONIZE
+		require 'daemons'
+		Daemons.daemonize
+	end
+
 	EventMachine::start_server '0.0.0.0', PORT, NNTPServer
 }
